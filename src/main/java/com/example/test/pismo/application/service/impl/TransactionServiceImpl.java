9983c3c6c380f6da133e58a51b31fb99b734d629
Marcello Cordeiro <maarc.hen@gmail.com>
2025-12-07 18:44:54 -0300

    feat: add unit tests for transaction creation

--- src/main/java/com/example/test/pismo/application/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/example/test/pismo/application/service/impl/TransactionServiceImpl.java
@@ -2,25 +2,25 @@ package com.example.test.pismo.application.service.impl;
 
 import com.example.test.pismo.application.dto.request.TransactionDTO;
 import com.example.test.pismo.application.exception.NotFoundException;
+import com.example.test.pismo.application.service.strategy.CreditOperation;
 import com.example.test.pismo.infrastructure.repository.AccountRepository;
 import com.example.test.pismo.infrastructure.repository.TransactionRepository;
 import com.example.test.pismo.domain.entities.TransactionEntity;
 import com.example.test.pismo.domain.factory.OperationFactory;
 import com.example.test.pismo.domain.port.TransactionService;
 import com.example.test.pismo.domain.port.Strategy.OperationStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-public class TransactionSeviceImpl implements TransactionService {
-    private final TransactionRepository transactionRepository;
+public class TransactionServiceImpl implements TransactionService {
     private final AccountRepository accountRepository;
     private final OperationFactory operationFactory;
 
 
-    public TransactionSeviceImpl(TransactionRepository transactionRepository, AccountRepository accountRepository, List<OperationStrategy> operationStrategy, OperationFactory operationFactory) {
-        this.transactionRepository = transactionRepository;
+    public TransactionServiceImpl(AccountRepository accountRepository, OperationFactory operationFactory) {
         this.accountRepository = accountRepository;
         this.operationFactory = operationFactory;
     }
